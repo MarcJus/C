7db9cca8eea3e761c4a9d5223dd874940daf4322
MarcJus <jusseaumemarc@gmail.com>
2019-01-28 20:46:52 +0100

    debut jeu pendu

--- main.c
+++ main.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
-
-int renvoieTriple(int nombre);
+#include <stdlib.h>
+#include <ctype.h>
 
 int main(int argc, char* argv[]){
-	int nombre=3;
-	int resultat=renvoieTriple(nombre);
-	printf("%d", resultat);
-	return 0;
+	 char lettre=0;
+	 char motSecret[]="MARRON";
+	 int lettreTrouvee[6]={0};
+	 int coupsRestants=0;
+	 printf("Bienvenue dans le Pendu!\n\n");
+	 while (coupsRestants>0 && !gagne(lettreTrouvee)){
+		 printf("\n\nIl vous reste %d coups à jouer", coupsRestants);
+		 printf("\nQuel est le mot secret ? ");
+
+		 for(int i=0; i<6; i++){
+			 if(lettreTrouvee[i]){
+				 printf("%c", motSecret);
+			 }else{
+				 printf("*");
+			 }
+		 }
+	 }
+
+     return 0;
 }
 
-int renvoieTriple(int nombre){
-	return nombre*3;
-}
